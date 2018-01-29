a3ed6a406abac5d50415ace75cba8edaef19bd32
Rgvsthefounder <35463676+Rgvsthefounder@users.noreply.github.com>
2018-01-29 14:29:38 +0530

    Add files via upload

---
+++ class2_oddOReven_Raghav.java
@@ -0,0 +1,13 @@
+package class2;
+
+public class class2_oddOReven_Raghav {
+	public static void main(String[]args) {
+		
+	int N= 110;
+	if (N%2==0)
+		System.out.print(+N+" is a even no.");
+	else
+		System.out.print(+N+" is an odd no.");
+}
+
+}
